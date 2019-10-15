997651c866be3752bebcddac94f5db2aa04ca762
nassorkhatib <cholonyo@163.com>
2019-10-15 22:14:19 +0800

    Initial commit

---
+++ faranhait.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+Fahrenheit = int(input("Enter a temperature in Fahrenheit: "))
+
+Celsius = (Fahrenheit - 32) * 5.0/9.0
+print ("Temperature:", Fahrenheit, "Fahrenheit = ", Celsius, " C")
